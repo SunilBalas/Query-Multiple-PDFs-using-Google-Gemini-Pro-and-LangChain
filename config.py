1e7f857d92cfcf46dca87123f2993d4d085c966e
SunilBalas <balassunil606@gmail.com>
2024-04-28 22:16:07 +0530

    setup the google api key in .env

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ from langchain_google_genai.embeddings import GoogleGenerativeAIEmbeddings
 from dotenv import load_dotenv
 
 # load all the environment variables
-# load_dotenv()
+load_dotenv()
 
 # configure Google Gemini API key
-genai.configure(api_key=os.environ.get('GOOGLE_API_KEY'))
+genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
 
 embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
