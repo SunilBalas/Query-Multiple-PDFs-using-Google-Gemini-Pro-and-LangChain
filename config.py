343748ad1be75c08bca8e6dafa51dfd1fcf6c9e1
SunilBalas <balassunil606@gmail.com>
2024-05-12 10:54:57 +0530

    setup the env using streamlit secrets

--- config.py
+++ config.py
@@ -1,13 +1,9 @@
-import os
 import google.generativeai as genai
+import streamlit as st
 
 from langchain_google_genai.embeddings import GoogleGenerativeAIEmbeddings
-from dotenv import load_dotenv
-
-# load all the environment variables
-load_dotenv()
 
 # configure Google Gemini API key
-genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
+genai.configure(api_key=st.secrets['GOOGLE_API_KEY'])
 
 embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
