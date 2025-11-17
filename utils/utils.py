3ca2ca15958136f118c061d0aa7c36192e43537a
Sunil Balas <64346665+SunilBalas@users.noreply.github.com>
2025-11-17 09:17:14 +0530

    Fix import path for RecursiveCharacterTextSplitter

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,5 @@
 from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from config import embeddings
